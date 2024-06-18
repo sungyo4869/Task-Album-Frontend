9f7ff701741e192fe60b38f762a0c78e941b9fa2
sungyo4869 <hchengjian1@gmail.com>
2024-06-19 03:45:05 +0900

    とりあえずホームページをつくった

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
-  return (
-    <main>
-      <p>Home Page</p>
-    </main>
-  );
+import { Grid, Typography, Button } from "@mui/material";
+import Image from "next/image";
+import Link from 'next/link';
+import Icon from '../public/icon.png'
+
+export default function Page() {
+    return (
+        <Grid container spacing={2} alignItems="center" justifyContent="center" style={{ height: '100vh' }}>
+            <Grid item xs={12} md={5} container justifyContent="center" alignItems="center" direction="column">
+                <Image
+                    src={Icon}
+                    alt="icon"
+                />
+                <Typography variant='h5'>
+                    開発者：城間華
+                </Typography>
+            </Grid>
+            <Grid item xs={12} md={6} textAlign="center">
+                <Typography variant='h2'>
+                    たすくあるばむ
+                </Typography>
+                <Typography variant='subtitle1'>
+                    心が温まるTODOアプリ
+                </Typography>
+                <Link href="/task-album" passHref>
+                    <Button variant='contained' sx={{ mt: 5 }}>
+                        はじめる
+                    </Button>
+                </Link>
+            </Grid>
+        </Grid>
+    )
 }
