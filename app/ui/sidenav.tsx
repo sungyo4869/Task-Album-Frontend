a19f328407a6ccd62e9277d352a46a8f443ea31f
sungyo4869 <hchengjian1@gmail.com>
2024-06-19 09:05:37 +0900

    sidenavの飛ぶパスいじった

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -8,7 +8,7 @@ export default function SideNav(){
         <div className="flex h-full flex-col px-3 py-4 md:px-2">
             <Link
                 className="mb-2 flex h-20 items-end justify-start rounded-md bg-orange-200 p-4 md:h-40"
-                href="/"
+                href="/task-album"
             >
                 <div className="w-32 text-black md:w-40">
                     <Logo />
